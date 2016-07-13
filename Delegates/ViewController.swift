f79b066fbb9b906ace15ca0552b193dd9a5a7a4a
Andrew Madsen <andrew@openreelsoftware.com>
2016-07-13 11:26:36 -0600

    Adds a text view and uses its delegate to disallow entering text containing vowels.

--- Delegates/ViewController.swift
+++ Delegates/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
+	var textView: UITextView!
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		// Create a text view
+		textView = UITextView(frame: CGRectInset(view.bounds, 22, 22))
+		view.addSubview(textView)
+		textView.layer.borderColor = UIColor.blackColor().CGColor
+		textView.layer.borderWidth = 2.0
+		
+		textView.delegate = self
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	// MARK: - UITextViewDelegate
+	
+	func textViewDidChange(textView: UITextView) {
+		print("Text did change to: \(textView.text)")
 	}
-
-
+	
+	let vowels = NSCharacterSet(charactersInString: "AEIOUaeiou")
+	func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+		if text.rangeOfCharacterFromSet(vowels) != nil {
+			return false
+		} else {
+			return true
+		}
+	}
+	
 }
 
