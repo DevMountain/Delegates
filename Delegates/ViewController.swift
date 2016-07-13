d6d0771572135078cc8150b0cf1d4802238d9321
Andrew Madsen <andrew@openreelsoftware.com>
2016-07-13 09:47:28 -0600

    Initial commit. Empty project.

---
+++ Delegates/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Delegates
+//
+//  Created by Andrew Madsen on 7/13/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
